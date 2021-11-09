cfb664fa74bf23ade159677a665fac8bd693a393
lucasrpb <lucasrpb@gmail.com>
2021-11-09 14:15:33 -0300

    Implemented reverse range....

--- src/main/scala/services/scalable/index/QueryableIndex.scala
+++ src/main/scala/services/scalable/index/QueryableIndex.scala
@@ -161,11 +161,9 @@ class QueryableIndex()(override implicit val ec: ExecutionContext, override val 
         override def compare(x: K, y: K): Int = 1
       } else new Ordering[K] {
         override def compare(x: K, y: K): Int = {
-          var r = order.compare(prefix.get, y.slice(0, prefix.get.length))
+          val r = order.compare(prefix.get, y.slice(0, prefix.get.length))
 
           if(r != 0) return r
-
-          //r = order.compare(term, y.slice(prefix.get.length, y.length))
 
           1
         }
@@ -299,7 +297,83 @@ class QueryableIndex()(override implicit val ec: ExecutionContext, override val 
     }
   }
 
+  protected def ranger(fromPrefix: Option[K], toPrefix: Option[K], from: K, to: K, includeFrom: Boolean, includeTo: Boolean): RichAsyncIterator[K, V] = {
+
+    val fromWord = if(fromPrefix.isDefined) fromPrefix.get ++ from else from
+    val toWord = if(toPrefix.isDefined) toPrefix.get ++ to else to
+
+    val sord = if(toPrefix.isEmpty) new Ordering[K] {
+      override def compare(x: K, y: K): Int = 1
+    } else new Ordering[K] {
+      override def compare(x: K, y: K): Int = {
+        val r = order.compare(toPrefix.get, y.slice(0, toPrefix.get.length))
+
+        if(r != 0) return r
+
+        1
+      }
+    }
+
+    new RichAsyncIterator[K, V] {
+
+      override def hasNext(): Future[Boolean] = {
+        if(!firstTime) return Future.successful(ctx.root.isDefined)
+        Future.successful(!stop && cur.isDefined)
+      }
+
+      def check(k: K): Boolean = {
+        ((includeFrom && order.gteq(k, fromWord) || order.gt(k, fromWord)) && (fromPrefix.isEmpty || order.gteq(k.slice(0, fromPrefix.get.length), fromPrefix.get))) &&
+          ((includeTo && order.lteq(k, toWord) || order.lt(k, toWord)) && (toPrefix.isEmpty || order.lteq(k.slice(0, toPrefix.get.length), toPrefix.get)))
+      }
+
+      override def next(): Future[Seq[Tuple[K, V]]] = {
+        if(!firstTime){
+          firstTime = true
+
+          return findPath(toWord)(sord).flatMap {
+            case None =>
+              cur = None
+              Future.successful(Seq.empty[Tuple[K, V]])
+
+            case Some(b) =>
+              cur = Some(b)
+
+              val filtered = b.tuples.filter{case (k, _) => check(k) }
+              //stop = filtered.isEmpty
+
+              println(s"${Console.GREEN_B}${b.tuples.map{case (k, _) => new String(k)}} filtered: ${filtered.length}${Console.RESET}\n")
+
+              if(filtered.isEmpty){
+                next()
+              } else {
+                Future.successful(checkCounter(filtered.filter{case (k, v) => filter(k, v)}))
+              }
+
+          }
+        }
+
+        $this.prev(cur.map(_.unique_id))(order).map {
+          case None =>
+            cur = None
+            Seq.empty[Tuple[K, V]]
+
+          case Some(b) =>
+            cur = Some(b)
+
+            val filtered = b.tuples.filter{case (k, _) => check(k) }
+            stop = filtered.isEmpty
+
+            checkCounter(filtered.filter{case (k, v) => filter(k, v) })
+        }
+      }
+    }
+  }
+
   def range(fromPrefix: Option[K], toPrefix: Option[K], from: K, to: K, includeFrom: Boolean, includeTo: Boolean, reverse: Boolean): RichAsyncIterator[K, V] = {
+
+    if(reverse){
+      return ranger(fromPrefix, toPrefix, from, to, includeFrom, includeTo)
+    }
 
     val fromWord = if(fromPrefix.isDefined) fromPrefix.get ++ from else from
     val toWord = if(toPrefix.isDefined) toPrefix.get ++ to else to
