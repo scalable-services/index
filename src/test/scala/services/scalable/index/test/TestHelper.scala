48549124e9051abd1cec5039797104580a853ffb
lucasrpb <Ghb7140#@!>
2023-06-04 16:41:55 -0300

    Some optimizations.

--- src/test/scala/services/scalable/index/test/TestHelper.scala
+++ src/test/scala/services/scalable/index/test/TestHelper.scala
@@ -1,6 +1,7 @@
 package services.scalable.index.test
 
 import com.datastax.oss.driver.api.core.CqlSession
+import com.datastax.oss.driver.api.core.config.{DefaultDriverOption, DriverConfigLoader}
 import org.cassandraunit.CQLDataLoader
 import org.cassandraunit.dataset.cql.ClassPathCQLDataSet
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper
@@ -60,10 +61,25 @@ object TestHelper {
   }
 
   def createCassandraSession(): CqlSession = {
+    val loader =
+      DriverConfigLoader.programmaticBuilder()
+        .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, java.time.Duration.ofSeconds(30))
+        .withInt(DefaultDriverOption.CONNECTION_MAX_REQUESTS, 31768)
+        .withInt(DefaultDriverOption.SESSION_LEAK_THRESHOLD, 1000)
+        .withString(DefaultDriverOption.PROTOCOL_VERSION, "V4")
+        .withString(DefaultDriverOption.RECONNECTION_POLICY_CLASS, "ExponentialReconnectionPolicy")
+        .withDuration(DefaultDriverOption.RECONNECTION_BASE_DELAY, java.time.Duration.ofSeconds(1))
+        .withDuration(DefaultDriverOption.RECONNECTION_MAX_DELAY, java.time.Duration.ofSeconds(10))
+        /*.startProfile("slow")
+        .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(30))
+        .endProfile()*/
+        .build()
+
     CqlSession
       .builder()
       .withKeyspace(KEYSPACE)
       .withAuthCredentials(CQL_USER, CQL_PWD)
+      .withConfigLoader(loader)
       .build()
   }
 
